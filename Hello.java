640cf027b198656c4ccbe5dc54b7e6743efd6713
cetc28 <forever7521@126.com>
2022-05-20 14:35:38 +0800

    dev branch forst modifdy

--- Hello.java
+++ Hello.java
@@ -2,6 +2,8 @@ public class Hello{
             public static void main(String[] args){
 		System.out.println("hello git");
 		System.out.println("modify git by cetc28");
+		System.out.println("modify git by dev");
+
 
 	}
 }
